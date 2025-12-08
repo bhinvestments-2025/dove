cf5a00bf71f3fce67a6ab946a520e7cbb01ef470
Kevin Valenzuela <office@gobhinvestments.com>
2025-12-09 01:36:42 +0800

    fix 404

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-
   images: {
     unoptimized: true,
   },
@@ -24,6 +21,21 @@ const nextConfig = {
   onDemandEntries: {
     maxInactiveAge: 10000,
     pagesBufferLength: 2,
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Frame-Options", value: "ALLOWALL" },
+          { key: "Content-Security-Policy", value: "frame-ancestors *;" },
+          { key: "Access-Control-Allow-Origin", value: process.env.CORS_ORIGINS || "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "*" },
+        ],
+      },
+    ];
   },
 };
 
